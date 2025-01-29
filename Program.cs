d9d423d9194bbd8807ab0c13db8385d6891e267f
Hadi-B-0924 <baih092024@gmail.com>
2025-01-29 17:49:38 +0100

    Grunnleggende C# Intro types

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace C__Trening.Types;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //For å definere en variabel, må vi først fortelle maskinen hvilken type denne skal være.
+        //Så gir vi variabelen et navn slik vi kan referere til den senere.
+        //Så gir vi variabelen en verdi, etter likhetstegnet. 
+        string myString  = "Hello World!"; //en string reprensenterer en tekst 
+        Console.WriteLine(myString);
+        //hele tall reprensenteres via en type som heter int.
+        int myWholeNumber = 20; // en int, har en minimumsverdi på -2^31 og en maksverdi på 2^31
+        /*int toLargeNumber = 49860214407294534071025435;  */ //vi må passe på at vi ikke assigner en for stor verdi for datatypen vi har valg.
+        Console.WriteLine(myWholeNumber);
+        float myFloat = 14.6f; //en gammel måte å reprensentere et desimaltall på. Generelt unøyaktig og lite brukt. 
+        Console.WriteLine(myFloat);
+    }
+}
